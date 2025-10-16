684ff88971cb4b4f794b880b77a5168f88c6a619
Toni Felizardo <antonio.felizardo.email@gmail.com>
2025-10-16 15:29:25 +0100

    fix

--- src/functions/maven.sh
+++ src/functions/maven.sh
@@ -19,10 +19,13 @@ maven_install()
     CWD=$(pwd)
     cd "${SUB_PROJECT_DIR}"
 
-    if [ "${is_release}" == "true" ];
+    if [ "${is_release}" = "true" ];
     then
         mvn versions:set -DnewVersion="${image_version}"
         mvn versions:commit
+    else
+        mvn versions:set -DnewVersion="${image_version}"
+        mvn versions:revert
     fi
     mvn -DskipTests clean install
 
